fb2d420c86c3e93ca2f86401312ed94ae9e9decc
DaNnyKg07 <parasyadavnirban999@gmail.com>
2021-02-19 16:17:22 +0530

    added app screenshot in README.md

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         key: _scaffoldKey,
         backgroundColor: Colors.teal,
